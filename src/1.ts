806660d54da6962fdbff7ca56aabf388f371d58a
Liubomyr Skorokhod <lyubko44@gmail.com>
2024-11-03 12:10:08 -0800

    done hw7

--- src/1.ts
+++ src/1.ts
@@ -5,15 +5,7 @@
 */
 
 class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+  constructor(private name: string, private age: number, private grade: string) {}
 }
 
 export {};
